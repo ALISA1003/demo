2b75c2bffc582fbed04181916d1a6e463624d808
79670 <alexanderisaev@bk.ru>
2022-04-08 15:44:59 +0300

    ...

---
+++ src/main/java/com/example/demo/MyControlle.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyControlle {
+
+    @Autowired
+    PizzaRepository repository;
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/add")
+    public @ResponseBody
+    String add(@RequestParam String name, @RequestParam Integer size){
+        Pizza pizza = new Pizza(name, size);
+        repository.save(pizza);
+        return "Saved successfull!: " + name + " " + size;
+    }
+    @GetMapping("/all")
+    public @ResponseBody Iterable<Pizza> allPizzas(){
+        return repository.findAll();
+    }
+
+}
